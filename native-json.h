506cb1d5b89b725aff321720484e3147a72ce137
Michael Ira Krufky <mkrufky@gmail.com>
2017-03-12 18:33:28 -0400

    native-json: fix 'Lines should be <= 80 characters long'
    Signed-off-by: Michael Ira Krufky <mkrufky@gmail.com>

--- native-json.h
+++ native-json.h
@@ -43,7 +43,8 @@ class JSON {
     return instance().parse(jsonString);
 #else
 #if (NODE_MAJOR_VERSION >= 7)
-    return v8::JSON::Parse(Nan::GetCurrentContext(), jsonString).ToLocalChecked();
+    return v8::JSON::Parse(Nan::GetCurrentContext(),
+      jsonString).ToLocalChecked();
 #else
     return v8::JSON::Parse(jsonString);
 #endif
@@ -55,7 +56,8 @@ class JSON {
 #if NATIVE_JSON_H_NEED_STRINGIFY
     return instance().stringify(jsonObject);
 #else
-    return v8::JSON::Stringify(Nan::GetCurrentContext(), jsonObject).ToLocalChecked();
+    return v8::JSON::Stringify(Nan::GetCurrentContext(),
+      jsonObject).ToLocalChecked();
 #endif
   }
 
