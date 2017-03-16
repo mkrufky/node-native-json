05326412d7d4d7e520f902e74cf3e69c3a80c3ca
Michael Ira Krufky <mkrufky@gmail.com>
2017-03-16 11:14:39 -0400

    native-json: v8::JSON::Parse() that takes no context returns v8::Local<v8::Value>
    Signed-off-by: Michael Ira Krufky <mkrufky@gmail.com>

--- native-json.h
+++ native-json.h
@@ -44,14 +44,15 @@ class JSON {
 #if NATIVE_JSON_H_NEED_PARSE
     return scope.Escape(instance().parse(jsonString));
 #else
-    Nan::MaybeLocal<v8::Value> result =
 #if (NODE_MAJOR_VERSION >= 7)
+    Nan::MaybeLocal<v8::Value> result =
       v8::JSON::Parse(Nan::GetCurrentContext(), jsonString);
-#else
-      v8::JSON::Parse(jsonString);
-#endif
+
     if (result.IsEmpty()) return v8::Local<v8::Value>();
     return scope.Escape(result.ToLocalChecked());
+#else
+    return scope.Escape(v8::JSON::Parse(jsonString));
+#endif
 #endif
   }
 
